7ad1b23994c1c2901de3503c008c411a1b5ce295
Josh Burd <Jburdt@gmail.com>
2022-06-09 18:28:15 -0400

    commit tree 3770e6cbfe5ac52b57af82d04373b92a91460618

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!'
+} else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks."
+} else if (distance > 2000 && distance < 2501){
+    return 'I will gladly take your thirty bucks.'
+} else if (distance > 2500) {
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+}
+function ternaryCheckCity(place){
+  if (place === 'NYC'){
+    return "Ok, sounds good."
+  } else if (place === 'Pittsburgh'){
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  if (money === "generous"){
+    return 'Thank you so much.'
+  } if (money === 'not as generous'){
+    return 'Thank you.'
+}   else {
+      return 'Bye.' 
+    }
 }
